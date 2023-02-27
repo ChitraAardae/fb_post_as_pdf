07e298751801daa1170154938b51d8f78d9ccc9d
Prasanna venkatesh <115142768+Prasanna-aardae@users.noreply.github.com>
2023-02-27 23:20:52 +0530

    Add jars and fonts

--- src/main/java/fb_html_converter/FBProjectConstants.java
+++ src/main/java/fb_html_converter/FBProjectConstants.java
@@ -8,7 +8,7 @@ public final class FBProjectConstants {
 	
 	public static final String POST_DATE_FORMAT = "MMM dd, yyyy hh:mm:ssa";	
 
-	public static final String PARENT_FOLDER = "sample";
+	public static final String PARENT_FOLDER = "sample/zipFile";
 	
 	public static final File LICENSE_FILE = new File("license/07c6ce0a1230f501bacc8ddebb307db52801d3092174dd8fb0465b18da38c74a.json");
 	
